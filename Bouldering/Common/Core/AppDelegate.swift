9c3bb55d8142aa38c2739ac06c1aade258754258
Po Sam <po@mobairu.nl>
2018-01-07 15:28:37 +0100

    Updated gym data and start initialiser at launch of app

--- Bouldering/Common/Core/AppDelegate.swift
+++ Bouldering/Common/Core/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        initialiser.start()
+        
         return true
     }
 
@@ -35,7 +37,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        initialiser.start()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
